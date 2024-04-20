b537b38deb2b7cc83ec310236487230536e937f4
Carlos Luis Meiriño Toledo <carlosluis911207@gmail.com>
2024-04-20 17:40:55 -0400

    multiLanguage

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	'success_store_data' => 'Datos creado corectamente.',
+	'success_list_data' => 'Respuesta exitosa.',
+	'success_update_data' => 'Datos modificado corectamente.',
+	'success_delete_data' => 'Datos eliminado corectamente.',
+	'error_500' => 'Error del servidor.',
+	'error_validation' => 'Errores de validación.',
+];
